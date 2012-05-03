f141fe7521ef769987417739aa7092f0dd1179de
Dave Abrahams <dave@boostpro.com>
2012-05-03 12:06:14 -0400

    Much massaging to support cluster building
    Also removed the temporary directory creation, since we already have temp
    directory provided by 0install; it's called
    environ['BUILDDIR']/os.getcwd().  We just create a subdirectory called
    ryppl_overlay.  That simplified code a bit.

--- zerocmake.py
+++ zerocmake.py
@@ -35,75 +35,84 @@ def cmake(args, **kw):
     _0launch(['--not-before=2.8.8', 'http://ryppl.github.com/feeds/cmake.xml'] + args, **kw)
 
 def run(args):
-    use_overlay = args.build_type in ('headers','preinstall','dbg')
-    component = 'dev' if args.build_type in ('headers','preinstall') else args.build_type
+    use_overlay = args.build_type in ('headers','preinstall','cluster','dbg')
+    component = 'dev' if args.build_type in ('headers','preinstall','cluster') else args.build_type
 
-    if args.build_type == 'preinstall':
+    if use_overlay:
+        SRCDIR = os.path.join(os.getcwd(),'ryppl_overlay')
+    else:
+        SRCDIR = env['SRCDIR']
+
+    if args.build_type in ('preinstall', 'cluster'):
         # since we actually want to save the built state in the cache,
         # use the DISTDIR to do the build.
         os.chdir(env['DISTDIR'])
 
     if use_overlay:
-        SRCDIR = tempfile.mkdtemp()
-    else:
-        SRCDIR = env['SRCDIR']
+        _msg('preparing source directory with CMakeLists.txt overlays...')
 
-    try:
-        if use_overlay:
-            _msg('preparing source directory with overlay...')
-            cmake(['-E', 'copy_directory', env['SRCDIR'], SRCDIR])
-            cmake(['-E', 'copy_directory', env['BOOST_CMAKELISTS_OVERLAY'], SRCDIR])
+        if args.cluster:
+            # Copy each individual member of the cluster into a subdirectory of SRCDIR
+            cluster = [x.split('=',1) for x in args.cluster]
+            for package,subdir in cluster:
+                cmake(['-E', 'copy_directory', env[package + '_SRCDIR'], os.path.join(SRCDIR,subdir)])
 
-
-        _msg('configuring...')
-        config_args = [
-                '-DCMAKE_MODULE_PATH=' + env['RYPPL_CMAKE_MODULE_PATH']
-              , '-DCMAKE_BUILD_TYPE='+ ('Debug' if component == 'dbg' else 'Release')
-              , '-DRYPPL_DISABLE_TESTS=1', '-DRYPPL_DISABLE_EXAMPLES=1'
-              ]
-
-        if component != 'doc':
-            config_args.append( '-DRYPPL_DISABLE_DOCS=1')
-
-        config_args.append(SRCDIR)
-        cmake(config_args)
-
-        if args.build_type == 'preinstall':
-            _msg('2nd configuration pass...')
-            cmake(config_args)
-
-        _msg('building...')
-        cmake(
-              ['--build', '.']
-            + {'doc':['--target','documentation']}.get(component,[]) )
-
-        if args.build_type == 'preinstall':
-            _msg('adjusting installation scripts...')
-            script_name = 'cmake_install.cmake'
-            for root, dirnames, filenames in os.walk('.'):
-                if script_name in filenames:
-                    script_path = os.path.join(root, script_name)
-                    old_script = open(script_path).read()
-                    
-                    assert build_dir in old_script, "Couldn't find %s in %s:\n" % (
-                        build_dir, script_path, old_script)
-
-                    build_dir = os.getcwd() + '/'
-                    
-                    new_script = old_script.replace(build_dir, '${RYPPL_PREINSTALL_DIR}/')
-                    open(script_name,'w').write(new_script)
+            _msg('cluster',cluster)
+            # Build the CmakeLists.txt file that includes them all
+            open(os.path.join(SRCDIR,'CMakeLists.txt'), 'w').write(
+                'cmake_minimum_required(VERSION 2.8.8 FATAL_ERROR)\n'
+                + '\n'.join('add_subdirectory('+x+')' for _,x in cluster)
+                )
         else:
-            _msg('installing...')
-            cmake(
-                  ['-DCOMPONENT='+component]
-                + ['-DCMAKE_INSTALL_PREFIX='+env['DISTDIR'], '-P', 'cmake_install.cmake']
-                , noreturn=not use_overlay)
+            cmake(['-E', 'copy_directory', env['SRCDIR'], SRCDIR])
 
-    except:
-        if use_overlay:
-            _msg('cleaning up temporary source directory...')
-            shutil.rmtree(SRCDIR, ignore_errors=True)
-        raise
+        cmake(['-E', 'copy_directory', env['BOOST_CMAKELISTS_OVERLAY'], SRCDIR])
+
+
+    _msg('configuring...')
+    config_args = ['-DCMAKE_VERBOSE_MAKEFILE=1',
+            '-DCMAKE_MODULE_PATH=' + env['RYPPL_CMAKE_MODULE_PATH']
+          , '-DCMAKE_BUILD_TYPE='+ ('Debug' if component == 'dbg' else 'Release')
+          , '-DRYPPL_DISABLE_TESTS=1', '-DRYPPL_DISABLE_EXAMPLES=1'
+          ]
+
+    if component != 'doc':
+        config_args.append( '-DRYPPL_DISABLE_DOCS=1')
+
+    config_args.append(SRCDIR)
+    cmake(['-DRYPPL_INITIAL_PASS=1']+config_args)
+
+    if args.build_type in ('preinstall','cluster'):
+        _msg('2nd configuration pass...')
+        cmake(['-DRYPPL_INITIAL_PASS=0']+config_args)
+
+    _msg('building...')
+    cmake(
+          ['--build', '.']
+        + {'doc':['--target','documentation']}.get(component,[]) )
+
+    if args.build_type in ('preinstall','cluster'):
+        _msg('adjusting installation scripts...')
+        script_name = 'cmake_install.cmake'
+        for root, dirnames, filenames in os.walk('.'):
+            if script_name in filenames:
+                script_path = os.path.join(root, script_name)
+                old_script = open(script_path).read()
+
+                build_dir = os.getcwd() + '/'
+
+                assert build_dir in old_script or root != '.' \
+                    , "Couldn't find %s in %s:\n" % (build_dir, script_path) + old_script
+
+                new_script = old_script.replace(build_dir, '${RYPPL_PREINSTALL_DIR}/')
+                open(script_name,'w').write(new_script)
+    else:
+        _msg('installing...')
+        cmake(
+              ['-DCOMPONENT='+component]
+            + ['-DCMAKE_INSTALL_PREFIX='+env['DISTDIR'], '-P', 'cmake_install.cmake']
+            , noreturn=not use_overlay)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -111,7 +120,12 @@ if __name__ == '__main__':
 
     parser.add_argument(
         'build_type'
-      , choices=('headers','bin','dev','doc','dbg','preinstall')
+      , choices=('headers','bin','dev','doc','dbg','preinstall','cluster')
       , help='Determines the type of build/install step to perform')
+
+    parser.add_argument(
+        'cluster'
+      , nargs='*'
+      , help='The elements of the cluster')
 
     run(parser.parse_args())
